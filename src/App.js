8eb99dc20251319302822a37749865fae7323a0b
yppeu <yppeu@naver.com>
2022-11-01 22:18:26 +0900

    1101

--- src/App.js
+++ src/App.js
@@ -4,72 +4,54 @@ import "./App.css";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail.js";
 import { useState } from "react";
-import imgurl from "./img/selly.jpg";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-
-        <div
-          className="main-bg"
-          style={{ backgroundImage: "url(" + imgurl + ")" }}
-        ></div>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Row>
-            <Col xs>First, but unordered</Col>
-            <div class="img-warp">
-            <img src="https://images.velog.io/images/sa1341/post/165510e4-8f0e-4525-8bab-05466a6308a3/react.png" />
-            </div>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-            <Col xs={{ order: 12 }}>Second, but last</Col>
-            <div class="img-warp">
-            <img 
-              src="https://images.velog.io/images/sa1341/post/165510e4-8f0e-4525-8bab-05466a6308a3/react.png"
-            />
-            </div>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-            <Col xs={{ order: 1 }}>Third, but second</Col>
-            <div class="img-warp">
-            <img
-              src="https://images.velog.io/images/sa1341/post/165510e4-8f0e-4525-8bab-05466a6308a3/react.png"
-            />
-            </div>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Row>
-        </Container>
-      </>
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
-
-      {/* <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#home">Cart</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
-      </Navbar> */}
+      </Navbar>
 
       <Routes>
-        <Route path="/" element={<div>메인페이지</div>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Row>
+                {shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i}></Card>;
+                })}
+              </Row>
+            </>
+          }
+        />
         <Route path="/detail" element={<Detail />} />
         <Route path="/about" element={<div>어바웃페이지</div>} />
       </Routes>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div class="img-warp">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
